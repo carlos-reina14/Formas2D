6d595e903176aa722baea78b4066384042b194cb
carlos-reina14 <carlosreinacarrascosa@gmail.com>
2025-05-30 13:41:55 +0200

    1.5 Pruebas formas geométricas en Main

--- Formas2D/Program.cs
+++ Formas2D/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Formas2D
 {
@@ -10,6 +6,83 @@ namespace Formas2D
     {
         static void Main()
         {
+            InicioPruebas();
+        }
+        
+        private static void InicioPruebas()
+        {
+            PruebaElipse();
+            PruebaCirculo();
+            PruebaRectangulo();
+            PruebaCuadrado();
+            PruebaTrianguloLados();
+            PruebaTrianguloBaseAltura();
+        }
+
+        private static void PruebaElipse()
+        {
+            var elipse = new Elipse(5, 3);
+            Console.WriteLine("Creación elipse 5 x 3");
+            Console.WriteLine($"Área de la elipse: {elipse.CalcularArea()}");
+            Console.WriteLine($"Perímetro de la elipse: {elipse.CalcularPerimetro()}");
+            Console.WriteLine("Pulsa cualquier tecla para continuar...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        private static void PruebaCirculo()
+        {
+            var circulo = new Circulo(5);
+            Console.WriteLine("Creación círculo con radio 5");
+            Console.WriteLine($"Área del círculo: {circulo.CalcularArea()}");
+            Console.WriteLine($"Perímetro del círculo: {circulo.CalcularPerimetro()}");
+            Console.WriteLine("Pulsa cualquier tecla para continuar...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        private static void PruebaRectangulo()
+        {
+            var rectangulo = new Rectangulo(4, 6);
+            Console.WriteLine("Creación rectángulo 4 x 6");
+            Console.WriteLine($"Área del rectángulo: {rectangulo.CalcularArea()}");
+            Console.WriteLine($"Perímetro del rectángulo: {rectangulo.CalcularPerimetro()}");
+            Console.WriteLine("Pulsa cualquier tecla para continuar...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        private static void PruebaCuadrado()
+        {
+            var cuadrado = new Cuadrado(7);
+            Console.WriteLine("Creación cuadrado 7x7x7x7");
+            Console.WriteLine($"Área del cuadrado: {cuadrado.CalcularArea()}");
+            Console.WriteLine($"Perímetro del cuadrado: {cuadrado.CalcularPerimetro()}");
+            Console.WriteLine("Pulsa cualquier tecla para continuar...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        private static void PruebaTrianguloLados()
+        {
+            var triangulo = new Triangulo(3, 4, 5);
+            Console.WriteLine("Creación triángulo con lados 3, 4 y 5");
+            Console.WriteLine($"Área del triángulo: {triangulo.CalcularArea()}");
+            Console.WriteLine($"Perímetro del triángulo: {triangulo.CalcularPerimetro()}");
+            Console.WriteLine("Pulsa cualquier tecla para continuar...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        private static void PruebaTrianguloBaseAltura()
+        {
+            var triangulo = new Triangulo(4, 3);
+            Console.WriteLine("Creación triángulo con base 4 y altura 3");
+            Console.WriteLine($"Área del triángulo: {triangulo.CalcularArea()}");
+            Console.WriteLine($"Perímetro del triángulo: {triangulo.CalcularPerimetro()}");
+            Console.WriteLine("Pulsa cualquier tecla para continuar...");
+            Console.ReadKey();
+            Console.Clear();
         }
     }
 }
